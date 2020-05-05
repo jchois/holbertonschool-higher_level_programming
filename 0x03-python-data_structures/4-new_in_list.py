c17f2b4db1506d3b7443e9bb0d1f9597857950af
jchois <choisjuliana@gmail.com>
2020-05-05 20:32:02 +0000

    test

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,1 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        my_list.copy()
-        return my_list
-    else:
-        my_list_2 = my_list.copy()
-        my_list_2[idx] = element
-        return my_list_2
