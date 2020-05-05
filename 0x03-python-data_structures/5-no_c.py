bae1086255213e265a8e7a7c3ed766741b93a42c
jchois <choisjuliana@gmail.com>
2020-05-05 21:11:14 +0000

    task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    if my_string:
+        new_list = list(my_string)
+        for i in new_list:
+            if i in "cC":
+                new_list.remove(i)
+        my_string = "".join(new_list)
+    return my_string
