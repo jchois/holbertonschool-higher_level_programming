3de8a43882700617c98b6e462f3e7bbdd60e5d48
jchois <choisjuliana@gmail.com>
2020-05-18 12:07:59 -0500

    space remove

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-
     for i in range(x):
         try:
             print(my_list[i], end="")
@@ -9,4 +8,3 @@ def safe_print_list_integers(my_list=[], x=0):
             break
     print()
     return count
-    
