127c6162e6a6df7af44b4d1373a6b510aab45ffd
jchois <choisjuliana@gmail.com>
2020-05-19 23:56:57 -0500

    task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Coordinates of a square"""
+
+
+class Square:
+    """Private instance attribute declared
+    Arguments:
+        size: size of a square
+    Raises:
+        TypeError/ValueError
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value) is not int or value[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value) is not int or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    """
+        Function that returns the current square area
+    """
+    def area(self):
+        return self.__size ** 2
+
+    """
+        Function that prints in stdout the square
+    """
+    def my_print(self):
+        new_list = []
+        for i in range(self.__size):
+            new_list.append("#" * self.__size)
+        print(*new_list, sep="\n")
