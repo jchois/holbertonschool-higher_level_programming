761c40ec65112ebd081d96e6d639fb0c8891973a
jchois <choisjuliana@gmail.com>
2020-05-19 22:21:05 -0500

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    def __init__(self, size):
+        """Private instance attribute declared
+        Arguments:
+            size: size of a square
+        """
+        self.__size = size
