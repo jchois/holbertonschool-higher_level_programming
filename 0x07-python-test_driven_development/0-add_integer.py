2a5ff48a6f6e48f0cdbf0331b6315a420398aa1d
jchois <choisjuliana@gmail.com>
2020-05-22 10:39:03 -0500

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Integers addition"""
+
+
+def add_integer(a, b=98):
+    """[summary]
+
+    Arguments:
+        a {[type]} -- [description]
+
+    Keyword Arguments:
+        b {int} -- [description] (default: {98})
+
+    Raises:
+        TypeError: [description]
+        TypeError: [description]
+
+    Returns:
+        [type] -- [description]
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    elif type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
