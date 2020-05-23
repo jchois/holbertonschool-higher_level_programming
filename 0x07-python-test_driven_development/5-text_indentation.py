3b752d26da7769e76fd22b74fc9d273a8b8b17f3
jchois <choisjuliana@gmail.com>
2020-05-22 20:07:40 -0500

    task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Function that prints a text with 2 new lines
+after each of these characters
+"""
+
+
+def text_indentation(text):
+    """[summary]
+
+    Arguments:
+        text {[str]}
+
+    Raises:
+        TypeError: [text must be a string]
+    """
+    if (text):
+        if type(text) is str:
+            char = ['.', '?', ':']
+            new_text = ""
+            for words in text:
+                new_text = new_text + words
+                if words in char:
+                    print(new_text.strip())
+                    print("")
+                    new_text = ""
+            print(new_text.strip(), end="")
+        else:
+            raise TypeError("text must be a string")
