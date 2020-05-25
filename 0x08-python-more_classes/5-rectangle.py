ebd748432f03e683acfaf3821f980092181203ce
jchois <choisjuliana@gmail.com>
2020-05-25 16:46:33 -0500

    task 5

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Rectangle Class"""
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        """[summary]
+
+        Keyword Arguments:
+            width {int} -- [description] (default: {0})
+            height {int} -- [description] (default: {0})
+        """
+        self.__width = width
+        self.__height = height
+
+    # width
+    @property
+    def width(self):
+        """init width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """validates"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    # height
+    @property
+    def height(self):
+        """init height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """validates"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """area func"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """perimeter func"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (2 * (self.width + self.height))
+
+    def __str__(self):
+        """str"""
+        if self.width == 0 or self.height == 0:
+            return ("")
+        width = "#" * self.width
+        show = width
+        for i in range(0, self.height - 1):
+            show += "\n" + width
+        return show
+
+    def __repr__(self):
+        """repr"""
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+
+    def __del__(self):
+        """del"""
+        print("{}".format("Bye rectangle..."))
