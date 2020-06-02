958e41cfc258d892a37b48ca850b5ef8ee0f29b1
jchois <choisjuliana@gmail.com>
2020-06-02 17:17:32 -0500

    task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""BaseGeometry"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
+
+    def area(self):
+        """[area]
+
+        Raises:
+            Exception: [area() is not implemented]
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """[summary]
+
+        Arguments:
+            name / value
+
+        Raises:
+            TypeError: <name> must be grater than 0
+            ValueError: <name> must be an integer
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """[Rectangle]
+
+    Arguments:
+        BaseGeometry - Inherit
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        Rectangle.integer_validator(self, "width", width)
+        Rectangle.integer_validator(self, "height", height)
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__width * self.__height
