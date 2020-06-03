869672cb0002c17831fc1c696fa440af5799cd49
jchois <choisjuliana@gmail.com>
2020-06-02 20:08:20 -0500

    task 8 fixed

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,32 +2,7 @@
 """BaseGeometry"""
 
 
-class BaseGeometry:
-    """empty class"""
-    pass
-
-    def area(self):
-        """[area]
-
-        Raises:
-            Exception: [area() is not implemented]
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """[summary]
-
-        Arguments:
-            name / value
-
-        Raises:
-            TypeError: <name> must be grater than 0
-            ValueError: <name> must be an integer
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -37,7 +12,7 @@ class Rectangle(BaseGeometry):
         BaseGeometry - Inherit
     """
     def __init__(self, width, height):
-        """[summary]
+        """[Init Rectangle]
 
         Arguments:
             width int
@@ -45,5 +20,5 @@ class Rectangle(BaseGeometry):
         """
         self.__width = width
         self.__height = height
-        Rectangle.integer_validator(self, "width", width)
-        Rectangle.integer_validator(self, "height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
