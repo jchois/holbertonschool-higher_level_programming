c9ffda7a22ac78df1fdc0d764f37f28e92a253d5
jchois <choisjuliana@gmail.com>
2020-06-02 21:35:13 -0500

    first advanced

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""MyInt class"""
+
+
+class MyInt(int):
+    def __init__(self, value):
+        """init"""
+        self.value = value
+    def __eq__(self, other):
+        """override =="""
+        return self.value != other
+    def __ne__(self, other):
+        """override !="""
+        return self.value == other
