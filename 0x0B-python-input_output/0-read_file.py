905b816edabdb725cf47298157b1d625131e8bdc
jchois <choisjuliana@gmail.com>
2020-06-03 18:00:15 -0500

    adding comments

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""function that reads a text file"""
+
+
 def read_file(filename=""):
+    """read_file
+
+    Keyword Arguments:
+        filename
+    """
     with open(filename, encoding="UTF-8") as myFile:
         print(myFile.read())
