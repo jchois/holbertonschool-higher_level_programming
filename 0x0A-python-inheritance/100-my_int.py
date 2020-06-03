4355999f1cb4a3cc36f4113be11ff4e203b7a29c
jchois <choisjuliana@gmail.com>
2020-06-02 21:36:38 -0500

    first advanced with spaces

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,9 +6,11 @@ class MyInt(int):
     def __init__(self, value):
         """init"""
         self.value = value
+
     def __eq__(self, other):
         """override =="""
         return self.value != other
+
     def __ne__(self, other):
         """override !="""
         return self.value == other
