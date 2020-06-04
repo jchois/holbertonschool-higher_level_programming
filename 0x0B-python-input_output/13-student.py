cd8c26d55a13cac7911f7f379fbbf685be27c2c3
jchois <choisjuliana@gmail.com>
2020-06-03 23:55:38 -0500

    task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+
+class Student:
+    """Class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to_json"""
+        new = {}
+        if attrs is None:
+            return self.__dict__
+        for i in attrs:
+            if hasattr(self, i):
+                new[i] = getattr(self, i)
+        return new
+
+    def reload_from_json(self, json):
+        for x, y in json.items():
+            if hasattr(self, i):
+                setattr(self, x, y)
