2718cf12c7385bd16a5b558095b35158f3f9d26f
jchois <choisjuliana@gmail.com>
2020-06-03 19:23:23 -0500

    adding comments

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """x"""
+    """appends a string at the end of the file"""
     with open(filename, "a", encoding="UTF-8") as myFile:
         return myFile.write(text)
