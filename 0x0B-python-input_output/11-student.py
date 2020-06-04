73e5fe37632bcb69d82c06b1d1a227f1c6043a4a
jchois <choisjuliana@gmail.com>
2020-06-03 23:22:11 -0500

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name)
+        """init
+        """
+        self.firs_name = firs_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """to_json"""
+        return self.__dict__
