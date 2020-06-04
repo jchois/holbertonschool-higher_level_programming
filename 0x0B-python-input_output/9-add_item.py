184c65d8bdb9a3ee4be91a2b586a11c7b5c5a780
jchois <choisjuliana@gmail.com>
2020-06-03 21:57:36 -0500

    import changed

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""x"""
-from json import argv
+"""
+adds all arguments to a Python list, and then save them to a file
+"""
+from sys import argv
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
@@ -11,4 +13,4 @@ except:
     list = []
 for i in range(1, len(argv)):
     list.append(argv[i])
-save_to_json_file(lst, "add_item.json")
+save_to_json_file(list, "add_item.json")
