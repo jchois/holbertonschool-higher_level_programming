bd9f49e2042a725f56e176a6e64f0bf7d077f499
jchois <choisjuliana@gmail.com>
2020-06-03 23:57:41 -0500

    task 13

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-"""Student Class"""
+"""[Student to JSON with filter]
+"""
 
 
 class Student:
-    """Class Student"""
-
+    """[Student]
+    """
     def __init__(self, first_name, last_name, age):
-        """initialize"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """to_json"""
-        new = {}
+        dic = {}
         if attrs is None:
             return self.__dict__
         for i in attrs:
             if hasattr(self, i):
-                new[i] = getattr(self, i)
-        return new
+                dic[i] = getattr(self, i)
+        return dic
 
     def reload_from_json(self, json):
-        """reload"""
-        for x, y in json.items():
+        for i, j in json.items():
             if hasattr(self, i):
-                setattr(self, x, y)
+                setattr(self, i, j)
