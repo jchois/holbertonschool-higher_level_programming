91645d5ccf30705b03390376d0328e9c55305125
jchois <choisjuliana@gmail.com>
2020-06-03 23:47:07 -0500

    task 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+
+class Student:
+    """Class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to_json"""
+        new = {}
+        if attrs is None:
+            return self.__dict__
+        for i in attrs:
+            if hasattr(self, i):
+                new[i] = getattr(self, i)
+        return new
