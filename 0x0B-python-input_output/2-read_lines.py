4859b60b25a121681f984abccf2f1ce4dca9b10d
jchois <choisjuliana@gmail.com>
2020-06-05 16:26:13 -0500

    task 2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,6 +6,10 @@ def read_lines(filename="", nb_lines=0):
     """x"""
     with open(filename, "r", encoding="UTF-8") as myFile:
         count = 0
-        if nb_lines <= 0:
+        for i in open(filename):
+            count += 1
+        if nb_lines <= 0 or nb_lines >= count:
             print(myFile.read(), end="")
-        
+        else:
+            for i in range(nb_lines):
+                print(myFile.readline(), end="")
