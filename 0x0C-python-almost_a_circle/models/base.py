460f403a0dc2a72d4aca7d8fbe7111f8935155b8
jchois <choisjuliana@gmail.com>
2020-06-08 15:19:47 -0500

    task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,7 +17,18 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """returns the JSON string representation"""
         if list_dictionaries is None or not list_dictionaries:
             return "[]"
         else:
             return dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation"""
+        newlist = []
+        if list_objs is not None:
+            for x in range(len(list_objs)):
+                newlist += [list_objs[0].to_dictionary()]
+            with open(cls.__name__ + ".json", "w") as f:
+                f.write(cls.to_json_string(newlist))
