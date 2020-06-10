fb25970a547d5b7adf95cb08dfa5b291b58fecda
jchois <choisjuliana@gmail.com>
2020-06-09 21:36:52 -0500

    Base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,7 +12,7 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects = Base.__nb_objects + 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
     @staticmethod
@@ -26,18 +26,18 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation"""
-        newlist = []
+        new_lst = []
         if list_objs is not None:
             for x in range(len(list_objs)):
-                newlist += [list_objs[0].to_dictionary()]
-            with open(cls.__name__ + ".json", "w") as f:
-                f.write(cls.to_json_string(newlist))
+                new_lst += [list_objs[x].to_dictionary()]
+        with open(cls.__name__ + ".json", 'w') as f:
+            f.write(cls.to_json_string(new_lst))
 
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation"""
         if json_string is None or not json_string:
-            return "[]"
+            return []
         else:
             return loads(json_string)
 
@@ -54,13 +54,13 @@ class Base:
     @classmethod
     def load_from_file(cls):
         """returns a list of instances"""
-        newlist = []
         new_file = cls.__name__ + ".json"
+        new_lst = []
         try:
-            with open(new_file, "r") as f:
-                newlist = cls.from_json_string(f.read())
-            for a, b in enumerate(newlist):
-                newlist[a] = cls.create(**newlist[a])
+            with open(new_file, 'r') as f:
+                new_lst = cls.from_json_string(f.read())
+            for a, _b in enumerate(new_lst):
+                new_lst[a] = cls.create(**new_lst[a])
         except:
             pass
-        return newlist
+        return new_lst
