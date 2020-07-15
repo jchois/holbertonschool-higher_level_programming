4685468137b09624568f66475d3d3f2ddf1d2593
jchois <choisjuliana@gmail.com>
2020-07-14 19:55:10 -0500

    change position of FREIGN KEY constraint

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,7 +4,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	state_id INT NOT NULL FOREIGN KEY REFERENCES hbtn_0d_usa.states(id),
+	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	UNIQUE (id)
+	UNIQUE (id),
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 );
