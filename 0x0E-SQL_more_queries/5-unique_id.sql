07360620a1777ee551ebf28c81ed24ed70b53625
jchois <choisjuliana@gmail.com>
2020-07-14 19:14:51 -0500

    table name changed

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- script that creates the table unique_id with constraint unique
 
-CREATE TABLE IF NOT EXISTS id_not_null(
+CREATE TABLE IF NOT EXISTS unique_id(
 	id INT DEFAULT 1,
 	name VARCHAR(256),
 	UNIQUE (id)
