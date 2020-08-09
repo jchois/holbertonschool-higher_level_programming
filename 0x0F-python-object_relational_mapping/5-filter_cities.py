06edb8240625bd415d9160f7d149e3c1c26777f3
jchois <choisjuliana@gmail.com>
2020-08-09 06:51:27 -0500

    task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM states\
+                 JOIN cities ON states.id=cities.state_id\
+                 WHERE states.name = %s ORDER BY id ASC", (argv[4], ))
+    rows = cur.fetchall()
+    new_list = []
+    for row in rows:
+        new_list.append(row[0])
+    print(", ".join(new_list))
+    cur.close()
+    db.close()
