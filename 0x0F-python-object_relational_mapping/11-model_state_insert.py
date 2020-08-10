b1e030a88b9299d970d40de1ec165135ed402890
jchois <choisjuliana@gmail.com>
2020-08-09 22:43:54 -0500

    new line added

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-script that adds the State object “Louisiana” to the database
+script that adds the State object “Louisiana”
+to the database
 """
 from sys import argv
 from model_state import Base, State
@@ -8,7 +9,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
