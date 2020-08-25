c5dd1911cd4df312b33af47c5dd2745ed3cc44bd
jchois <choisjuliana@gmail.com>
2020-08-24 21:30:31 -0500

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""s"""
+
+import urllib.request
+import urllib.parse
+from sys import argv
+
+
+if __name__ == '__main__':
+    data = urllib.parse.urlencode({'email': argv[2]})
+    data = data.encode('ascii')
+    url = argv[1]
+
+    with urllib.request.urlopen(url, data) as response:
+        print(response.read().decode())
