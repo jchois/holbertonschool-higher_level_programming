ed0970aa90eb76d3c4e909a3739d0a1a4efe703a
jchois <choisjuliana@gmail.com>
2020-09-15 16:47:59 -0500

    Fixing conditional

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const myVar = process.argv.slice(2);
 
-if (myVar.length === 0) {
+if (myVar.length < 3) {
   console.log('No argument');
-} else if (myVar.length === 1) {
+} else if (myVar.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Argument found');
