b65c0aa3387db58f9ba8925b05651d8183011fe2
jchois <choisjuliana@gmail.com>
2020-09-15 17:39:35 -0500

    Fixing task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-if (parseInt(procces.argv[2])) {
-	console.log('My number: ' + procces.argv[2]);
+if (parseInt(process.argv[2])) {
+  console.log('My number: ' + process.argv[2]);
 } else {
-	console.log('Not a number');
+  console.log('Not a number');
 }
