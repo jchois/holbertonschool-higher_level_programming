564501b1602e202a05b19fac8ea519d37d826d8f
jchois <choisjuliana@gmail.com>
2020-09-15 18:30:08 -0500

    Adding else

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,6 +2,10 @@
 
 const size = process.argv[2];
 
-for (let i = 0; i < size; i++) {
-  console.log('X'.repeat(size));
+if (size) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+} else {
+  console.log('Missing size');
 }
