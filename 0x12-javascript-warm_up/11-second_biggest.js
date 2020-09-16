bb34983228c1c75cc0fba2eef83ec5cba39474cf
jchois <choisjuliana@gmail.com>
2020-09-15 19:25:49 -0500

    Task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  const parsedArr = process.argv
+    .splice(2, process.argv.length - 1)
+    .sort(function (a, b) {
+      return a - b;
+    })
+    .map(function (number) {
+      return parseInt(number);
+    });
+  console.log(parsedArr.reverse()[1]);
+}
