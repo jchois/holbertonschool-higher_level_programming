cf567fcadcfd8232475e8521da816b39679ae7f4
jchois <choisjuliana@gmail.com>
2020-09-22 15:12:33 -0500

    Task 4 - search character

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,12 +3,12 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
-  if (error) {console.error(error);}
+  if (error) { console.error(error); }
   const films = JSON.parse(body).results;
   let count = 0;
-  films.forEach(function (films) {
+  films.forEach(films => {
     const char = films.characters;
-    char.forEach(function (char) {
+    char.forEach(char => {
       if (char.includes('/18')) {
         count++;
       }
